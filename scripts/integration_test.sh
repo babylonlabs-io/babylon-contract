f6ac891a2af9f592e7775026e77bf77064a931f0
bap2pecs <111917526+bap2pecs@users.noreply.github.com>
2024-06-11 00:17:46 -0400

    Fix build-optimizer.sh to properly generate code for arm64 (#142)

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
 ./scripts/optimizer.sh
-M=$(uname -m)
-
-if [ "$M" = "arm64" ]
-then
-  for A in ./artifacts/*-aarch64.wasm
-  do
-    B=$(basename $A -aarch64.wasm)
-    cp "$A" ./artifacts/"$B".wasm
-  done
-fi
 
 cargo test --test integration
