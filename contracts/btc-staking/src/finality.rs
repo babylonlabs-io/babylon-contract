bd950efc62d6f64568e927d3bb448c0285e0c61e
Mauro Lacy <mauro@babylonlabs.io>
2024-08-03 06:38:48 +0200

    Improve tests

--- contracts/btc-staking/src/finality.rs
+++ contracts/btc-staking/src/finality.rs
@@ -1025,7 +1025,7 @@ pub(crate) mod tests {
         let msg = ExecuteMsg::SubmitFinalitySignature {
             fp_pubkey_hex: pk_hex.clone(),
             height: submit_height,
-            pub_rand: pub_rand_one.into(),
+            pub_rand: pub_rand_one.clone().into(),
             proof: proof.into(),
             block_hash: add_finality_signature_2.block_app_hash.to_vec().into(),
             signature: Binary::new(add_finality_signature_2.finality_sig.to_vec()),
@@ -1049,8 +1049,16 @@ pub(crate) mod tests {
         assert_eq!(1, res.events.len());
         // Assert the slashing event is proper
         assert_eq!(res.events[0].ty, "slashed_finality_provider".to_string());
-        // Use the extracted secret key for simplicity
-        let btc_sk = hex::decode(res.events[0].attributes[7].value.clone()).unwrap();
+        // Extract the secret key to compare it
+        let btc_sk = eots::extract(
+            &eots::PublicKey::from_hex(&pk_hex).unwrap(),
+            &pub_rand_one,
+            &add_finality_signature.block_app_hash,
+            &finality_signature,
+            &add_finality_signature_2.block_app_hash,
+            &add_finality_signature_2.finality_sig,
+        )
+        .unwrap();
         assert_eq!(
             res.messages[0],
             SubMsg::new(WasmMsg::Execute {
@@ -1058,7 +1066,7 @@ pub(crate) mod tests {
                 msg: to_json_binary(&babylon_contract::ExecuteMsg::Slashing {
                     fp_btc_pk: btc_pk.clone(),
                     block_height: submit_height,
-                    secret_key: btc_sk
+                    secret_key: btc_sk.to_bytes()
                 })
                 .unwrap(),
                 funds: vec![]
