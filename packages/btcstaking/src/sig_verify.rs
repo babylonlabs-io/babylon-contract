1485e7736e2c89337debee28184a94f1962c0811
Runchao Han <me@runchao.rocks>
2024-08-23 15:15:08 +1000

    compile

--- packages/btcstaking/src/sig_verify.rs
+++ packages/btcstaking/src/sig_verify.rs
@@ -2,13 +2,22 @@ use bitcoin::hashes::Hash;
 use bitcoin::key::Secp256k1;
 use bitcoin::secp256k1::schnorr::Signature as SchnorrSignature;
 use bitcoin::secp256k1::Message;
+use bitcoin::secp256k1::Parity;
+use bitcoin::secp256k1::PublicKey;
 use bitcoin::sighash::{Prevouts, SighashCache};
 use bitcoin::Transaction;
 use bitcoin::{Script, TxOut, XOnlyPublicKey};
-use schnorr_fun::adaptor::{Adaptor, EncryptedSignature as AdaptorSignature};
-use schnorr_fun::{Message as ASigMessage, Schnorr};
-use secp256kfun::{marker::*, Point, Scalar};
-use sha2::Sha256;
+use k256::elliptic_curve::sec1::ToEncodedPoint;
+use k256::pkcs8::der::Choice;
+use k256::{
+    elliptic_curve::{
+        ops::{MulByGenerator, Reduce},
+        point::{AffineCoordinates, DecompressPoint},
+        PrimeField,
+    },
+    AffinePoint, ProjectivePoint, Scalar, U256,
+};
+use sha2::{Digest, Sha256};
 
 /// MODNSCALAR_SIZE is the size of a scalar on the secp256k1 curve
 const MODNSCALAR_SIZE: usize = 32;
@@ -67,22 +76,84 @@ pub fn verify_transaction_sig_with_output(
         .map_err(|e| e.to_string())
 }
 
+pub struct AdaptorSignature {
+    R: ProjectivePoint,
+    s_hat: Scalar,
+    needs_negation: bool,
+}
+
+const CHALLENGE_TAG: &[u8] = b"BIP0340/challenge";
+
+// Adapted from https://github.com/RustCrypto/elliptic-curves/blob/520f67d26be1773bd600d05796cc26d797dd7182/k256/src/schnorr.rs#L181-L187
+fn tagged_hash(tag: &[u8]) -> Sha256 {
+    let tag_hash = Sha256::digest(tag);
+    let mut digest = Sha256::new();
+    // The hash is in sha256d, so we need to hash twice
+    digest.update(tag_hash);
+    digest.update(tag_hash);
+    digest
+}
+
+#[allow(non_snake_case)]
 fn verify_adaptor_sig(
     pub_key: &XOnlyPublicKey,
     enc_key: &XOnlyPublicKey,
     msg: [u8; 32],
     asig: &AdaptorSignature,
 ) -> Result<(), String> {
-    let verification_key = Point::from(*pub_key);
-    let enc_key = Point::from(*enc_key);
-    let parsed_msg = ASigMessage::<Public>::raw(&msg);
-    let verifier = Schnorr::<Sha256>::verify_only();
-    verifier
-        .verify_encrypted_signature(&verification_key, &enc_key, parsed_msg, asig)
-        .then_some(())
-        .ok_or("failed to verify the adaptor signature".to_string())
+    // Convert public keys to points
+    let pk = k256::PublicKey::from_sec1_bytes(&pub_key.serialize()).map_err(|e| e.to_string())?;
+    let P = pk.to_projective();
+    let ek = k256::PublicKey::from_sec1_bytes(&enc_key.serialize()).map_err(|e| e.to_string())?;
+    let T = ek.to_projective();
+
+    // Calculate R' = R - T (or R + T if negation is needed)
+    let mut R_hat = asig.R;
+    if asig.needs_negation {
+        R_hat = R_hat + T;
+    } else {
+        R_hat = R_hat - T;
+    }
+    // Convert R' to affine coordinates
+    let R_hat = R_hat.to_affine();
+
+    // Calculate e = tagged_hash("BIP0340/challenge", bytes(R) || bytes(P) || m)
+    // mod n
+    let R_bytes = R_hat.x();
+    let p_bytes = pub_key.serialize();
+    let e = <Scalar as Reduce<U256>>::reduce_bytes(
+        &tagged_hash(CHALLENGE_TAG)
+            .chain_update(R_bytes)
+            .chain_update(p_bytes)
+            .chain_update(msg)
+            .finalize(),
+    );
+
+    // Calculate expected R' = s'*G - e*P
+    let s_hat_g = ProjectivePoint::mul_by_generator(&asig.s_hat);
+    let e_p = P * e;
+    let expected_R_hat = s_hat_g - e_p;
+
+    // // Ensure expected R' is not the point at infinity
+    // if expected_R_hat.is_identity() {
+    //     return Err("Expected R' is the point at infinity".to_string());
+    // }
+
+    // Ensure expected R'.y is even
+    let expected_R_hat = expected_R_hat.to_affine();
+    if expected_R_hat.y_is_odd().into() {
+        return Err("Expected R'.y is odd".to_string());
+    }
+
+    // Ensure R' == expected R'
+    if R_hat != expected_R_hat {
+        return Err("R' does not match expected R'".to_string());
+    }
+
+    Ok(())
 }
 
+#[allow(non_snake_case)]
 pub fn new_adaptor_sig(asig_bytes: &[u8]) -> Result<AdaptorSignature, String> {
     if asig_bytes.len() != ADAPTOR_SIGNATURE_SIZE {
         return Err(format!(
@@ -91,15 +162,27 @@ pub fn new_adaptor_sig(asig_bytes: &[u8]) -> Result<AdaptorSignature, String> {
             asig_bytes.len()
         ));
     }
-    let (r, _) = Point::from_slice(&asig_bytes[0..JACOBIAN_POINT_SIZE])
-        .ok_or("failed to get R in an adaptor signature")?
-        .into_point_with_even_y();
-    let s_hat =
-        Scalar::from_slice(&asig_bytes[JACOBIAN_POINT_SIZE..JACOBIAN_POINT_SIZE + MODNSCALAR_SIZE])
-            .ok_or("failed to get s_hat in an adaptor signature")?;
+    // get R
+    let is_y_odd = asig_bytes[0] == 0x03;
+    let R_option = AffinePoint::decompress(
+        k256::FieldBytes::from_slice(&asig_bytes[1..JACOBIAN_POINT_SIZE]),
+        k256::elliptic_curve::subtle::Choice::from(is_y_odd as u8),
+    );
+    let R = if R_option.is_some().into() {
+        R_option.unwrap().into()
+    } else {
+        return Err("Failed to decompress R point".to_string());
+    };
+
+    // get s_hat
+    let s_hat_bytes = &asig_bytes[JACOBIAN_POINT_SIZE..JACOBIAN_POINT_SIZE + MODNSCALAR_SIZE];
+    let s_hat_field_bytes = *k256::FieldBytes::from_slice(s_hat_bytes);
+    let s_hat = Scalar::from_repr_vartime(s_hat_field_bytes)
+        .ok_or("failed to get s_hat in an adaptor signature")?;
+
     let needs_negation = asig_bytes[JACOBIAN_POINT_SIZE + MODNSCALAR_SIZE] == 0x01;
     Ok(AdaptorSignature {
-        R: r,
+        R: R,
         s_hat,
         needs_negation,
     })
