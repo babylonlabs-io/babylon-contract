6557bcddfa45877bd7837ce661db53640a47a23e
Mauro Lacy <mauro@babylonlabs.io>
2024-08-27 08:14:54 +0200

    IBC slashing message with protobuf

--- contracts/babylon/src/ibc.rs
+++ contracts/babylon/src/ibc.rs
@@ -178,16 +178,14 @@ pub(crate) mod ibc_packet {
     use super::*;
     use crate::state::config::CONFIG;
     use babylon_apis::btc_staking_api::SlashedBtcDelegation;
-    use babylon_apis::ibc_consumer::{consumer_packet_data, ConsumerPacketData};
-    use babylon_apis::{
-        btc_staking_api::{
-            ActiveBtcDelegation, BtcUndelegationInfo, CovenantAdaptorSignatures,
-            FinalityProviderDescription, NewFinalityProvider, ProofOfPossessionBtc, SignatureInfo,
-            UnbondedBtcDelegation,
-        },
-        ibc_consumer,
+    use babylon_apis::btc_staking_api::{
+        ActiveBtcDelegation, BtcUndelegationInfo, CovenantAdaptorSignatures,
+        FinalityProviderDescription, NewFinalityProvider, ProofOfPossessionBtc, SignatureInfo,
+        UnbondedBtcDelegation,
     };
     use babylon_proto::babylon::btcstaking::v1::BtcStakingIbcPacket;
+    use babylon_proto::babylon::zoneconcierge::v1::zoneconcierge_packet_data::Packet::ConsumerSlashing;
+    use babylon_proto::babylon::zoneconcierge::v1::ConsumerSlashingIbcPacket;
     use cosmwasm_std::{to_json_binary, Decimal, IbcChannel, IbcMsg, WasmMsg};
     use std::str::FromStr;
 
@@ -365,16 +363,16 @@ pub(crate) mod ibc_packet {
         block_height: u64,
         secret_key: &[u8],
     ) -> Result<IbcMsg, ContractError> {
-        let packet = ConsumerPacketData {
-            packet: consumer_packet_data::Packet::Slashing(ibc_consumer::Slashing {
-                fp_btc_pk: fp_btc_pk.to_vec(),
+        let packet = ZoneconciergePacketData {
+            packet: Some(ConsumerSlashing(ConsumerSlashingIbcPacket {
+                fp_btc_pk: fp_btc_pk.to_vec().into(),
                 block_height,
-                secret_key: secret_key.to_vec(),
-            }),
+                secret_key: secret_key.to_vec().into(),
+            })),
         };
         let msg = IbcMsg::SendPacket {
             channel_id: channel.endpoint.channel_id.clone(),
-            data: to_json_binary(&packet)?,
+            data: Binary::new(packet.encode_to_vec()),
             timeout: packet_timeout(env),
         };
         Ok(msg)
--- packages/apis/src/lib.rs
+++ packages/apis/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod btc_staking_api;
 pub mod error;
 pub mod finality_api;
-pub mod ibc_consumer;
 mod validate;
 
 use cosmwasm_std::{Addr, Binary, CustomQuery, QueryRequest, WasmQuery};
