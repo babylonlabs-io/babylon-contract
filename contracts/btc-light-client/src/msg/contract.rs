556f362f6419b63ec89ba10601fdc1db4a38b419
Runchao Han <me@runchao.rocks>
2025-03-06 14:49:15 +1100

    feat: BTC light client contract (#120)
    First step of
    https://github.com/babylonlabs-io/babylon-contract/issues/53
    
    This PR creates a new BTC light client contract. The code is migrated
    from the existing Babylon contract. In particular, this PR
    
    - [x] creates new BTC light client contract
    - [x] migrates BTC light client functionalities from Babylon contract to
    BTC light client contract
    - [x] migrates queries / exec msgs
    - [x] migrates tests
    
    TODOs after this PR
    
    - [ ] strip BTC light client code in Babylon contract
    - [ ] wire up instantiation of BTC light client in Babylon contract
    - [ ] multi-tests
    - [ ] e2e tests in Babylon SDK

---
+++ contracts/btc-light-client/src/msg/contract.rs
@@ -0,0 +1,54 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{StdError, StdResult};
+
+use crate::msg::btc_header::{BtcHeader, BtcHeaderResponse, BtcHeadersResponse};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub network: babylon_bitcoin::chain_params::Network,
+    pub btc_confirmation_depth: u32,
+    pub checkpoint_finalization_timeout: u32,
+}
+
+impl InstantiateMsg {
+    pub fn validate(&self) -> StdResult<()> {
+        if self.btc_confirmation_depth == 0 {
+            return Err(StdError::generic_err(
+                "BTC confirmation depth must be greater than 0",
+            ));
+        }
+        if self.checkpoint_finalization_timeout == 0 {
+            return Err(StdError::generic_err(
+                "Checkpoint finalization timeout must be greater than 0",
+            ));
+        }
+        Ok(())
+    }
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    /// Initialize the BTC light client with a list of consecutive headers
+    InitBtcLightClient { headers: Vec<BtcHeader> },
+    /// Update the BTC light client with a list of consecutive headers
+    BtcHeaders { headers: Vec<BtcHeader> },
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    #[returns(BtcHeaderResponse)]
+    BtcBaseHeader {},
+    #[returns(BtcHeaderResponse)]
+    BtcTipHeader {},
+    #[returns(BtcHeaderResponse)]
+    BtcHeader { height: u32 },
+    #[returns(BtcHeaderResponse)]
+    BtcHeaderByHash { hash: String },
+    #[returns(BtcHeadersResponse)]
+    BtcHeaders {
+        start_after: Option<u32>,
+        limit: Option<u32>,
+        reverse: Option<bool>,
+    },
+}
