d6709110ec71ec897a3952e62f1e84e9406175cc
Mauro Lacy <mauro@babylonlabs.io>
2025-05-05 09:48:08 +0200

    Rename utils / storage keys access

--- contracts/babylon/src/utils/consumer_header_chain.rs
+++ contracts/babylon/src/utils/consumer_header_chain.rs
@@ -7,26 +7,28 @@ use tendermint_proto::crypto::ProofOps;
 /// verify_tx_in_block verifies whether a tx with tx_hash is committed to
 /// the Merkle root hash of a Tendermint block
 pub fn verify_consumer_header_in_epoch(
-    cz_header: &IndexedHeader,
+    consumer_header: &IndexedHeader,
     epoch: &Epoch,
-    proof_cz_header_in_epoch: &ProofOps,
+    proof_consumer_header_in_epoch: &ProofOps,
 ) -> Result<(), error::ConsumerHeaderChainError> {
-    // verify CZ header has correct epoch number
-    if cz_header.babylon_epoch != epoch.epoch_number {
+    // Verify that the Consumer header has the correct epoch number
+    if consumer_header.babylon_epoch != epoch.epoch_number {
         return Err(error::ConsumerHeaderChainError::EpochNumberError {});
     }
 
-    // Ensure the CZ header is committed to the app_hash of the sealer header
+    // Ensure the Consumer header is committed to the app_hash of the sealer header
     let root = &epoch.sealer_app_hash;
-    let cz_header_key =
-        super::cosmos_store::get_cz_header_key(&cz_header.consumer_id, cz_header.height);
-    let cz_header_bytes = cz_header.encode_to_vec();
+    let consumer_header_key = super::cosmos_store::get_consumer_header_key(
+        &consumer_header.consumer_id,
+        consumer_header.height,
+    );
+    let consumer_header_bytes = consumer_header.encode_to_vec();
     super::cosmos_store::verify_store(
         root,
         super::cosmos_store::ZONECONCIERGE_STORE_KEY,
-        &cz_header_key,
-        &cz_header_bytes,
-        proof_cz_header_in_epoch,
+        &consumer_header_key,
+        &consumer_header_bytes,
+        proof_consumer_header_in_epoch,
     )
     .map_err(|_| error::ConsumerHeaderChainError::ProofError {})?;
 
--- contracts/babylon/src/utils/cosmos_store.rs
+++ contracts/babylon/src/utils/cosmos_store.rs
@@ -23,7 +23,7 @@ pub fn get_valset_key(epoch_number: u64) -> Vec<u8> {
     epoch_valset_key
 }
 
-pub fn get_cz_header_key(chain_id: &String, height: u64) -> Vec<u8> {
+pub fn get_consumer_header_key(chain_id: &String, height: u64) -> Vec<u8> {
     // https://github.com/babylonlabs-io/babylon/blob/8638c950fd2de1ac5dc69a8b9f710c1fa720c155/x/zoneconcierge/types/keys.go#L33
     let mut key = [0x13].to_vec();
     key.extend(chain_id.as_bytes());
